cf4ac048f5dabdc3403b03b71af4018b593d8bfb
Kenny Rachuonyo <krmboya@gmail.com>
2022-07-05 14:54:33 +0300

    some dummy text

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+	  Let's print some free money like the central banks
         </div>
 
         <button className="waveButton" onClick={wave}>
-          Wave at Me
+          Let's go
         </button>
       </div>
     </div>
