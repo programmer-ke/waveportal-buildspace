467ee0d8938761bafd4c813fad4c9c6a793bd8d0
Kenny Rachuonyo <krmboya@gmail.com>
2022-07-07 14:36:46 +0300

    check if wallet is connected

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect } from "react";
 import './App.css';
 
 export default function App() {
 
-  const wave = () => {
-    
-  }
+    const checkIfWalletIsConnected = () => {
+	const { ethereum } = window;
+	
+	if (!ethereum) {
+	    console.log("Make sure you have metamask!");
+	} else {
+	    console.log("We have the ethereum object", ethereum);
+	}
+    };
+
+    useEffect(() => {
+	checkIfWalletIsConnected();
+    }, []);
   
-  return (
-    <div className="mainContainer">
+    return (
+      <div className="mainContainer">
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
+	<div className="dataContainer">
+	  <div className="header">
+	  👋 Hey there!
+	  </div>
 
-        <div className="bio">
-	  Let's print some free money like the central banks
-        </div>
+	  <div className="bio">
+	    Let's print some free money like the central banks
+	  </div>
 
-        <button className="waveButton" onClick={wave}>
-          Let's go
-        </button>
+	  <button className="waveButton" onClick={wave}>
+	    Let's go
+	  </button>
+	</div>
       </div>
-    </div>
-  );
+    );
 }
