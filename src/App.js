ea9d4704afde11ae5fef0030d4b9c4b58ef8f623
Kenny Rachuonyo <krmboya@gmail.com>
2022-07-14 13:33:48 +0300

    check if account access is authorized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 export default function App() {
 
-    const checkIfWalletIsConnected = () => {
-	const { ethereum } = window;
-	
-	if (!ethereum) {
-	    console.log("Make sure you have metamask!");
-	} else {
-	    console.log("We have the ethereum object", ethereum);
+    const [currentAccount, setCurrentAccount ] = useState("");
+
+    const checkIfWalletIsConnected = async () => {
+
+	try {
+	    const { ethereum } = window;
+	    
+	    if (!ethereum) {
+		console.log("Make sure you have metamask!");
+		return;
+	    } else {
+		console.log("We have the ethereum object", ethereum);
+	    }
+
+	    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+	    if (accounts.length !== 0) {
+		const account = accounts[0];
+		console.log("Found an authorized account:", account);
+		setCurrentAccount(account);
+	    } else {
+		console.log("No authorized account found");
+	    }
+	    
+	} catch (error) {
+	    console.log(error);
 	}
+	
     };
 
     useEffect(() => {
@@ -22,7 +42,7 @@ export default function App() {
 
 	<div className="dataContainer">
 	  <div className="header">
-	  👋 Hey there!
+	  👋 Hey there! 👋
 	  </div>
 
 	  <div className="bio">
