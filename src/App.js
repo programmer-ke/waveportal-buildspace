310f4f784a6995a408bf1af987b1a95b455d546f
Kenny Rachuonyo <krmboya@gmail.com>
2022-11-19 16:44:58 +0300

    retrieve waves after posting a new one

--- src/App.js
+++ src/App.js
@@ -118,6 +118,8 @@ export default function App() {
 		
 		count = await wavePortalContract.getTotalWaves();
 		console.log("Retrieved total wave count...", count.toNumber());
+
+		getAllWaves();
 	    } else {
 		console.log("Metamask not detected!");
 	    }
